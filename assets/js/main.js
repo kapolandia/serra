616921e0bbc9f07813e116b0e6e4e871582021f3
kapolandia <paykapo4@gmail.com>
2024-05-03 11:51:13 +0200

    fix cards 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,14 @@ document.getElementById("cards").onmousemove = e => {
     card.style.setProperty("--mouse-y", `${y}px`);
   };
 }
+
+document.getElementById("cards2").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
